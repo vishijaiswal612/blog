0b9945887270c279efdba1215c0a8d43fc5ea870
Vishi Jaiswal <vishi.jaiswal@gemsessence.com>
2022-12-01 16:34:41 +0530

    new project push to github with model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_131146) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_110004) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
